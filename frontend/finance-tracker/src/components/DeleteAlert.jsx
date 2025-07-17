3ce2ee1420f06711368b1cc7afadd4001a5b5346
Sylester-Oputa <soputa42@gmail.com>
2025-07-17 23:45:28 +0100

    commit

--- frontend/finance-tracker/src/components/DeleteAlert.jsx
+++ frontend/finance-tracker/src/components/DeleteAlert.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const DeleteAlert = ({ content, onDelete }) => {
   return (
     <div>
-      <p className="text-sm text-white">{content}</p>
+      <p className="text-sm">{content}</p>
 
       <div className="flex justify-end mt-6">
         <button
