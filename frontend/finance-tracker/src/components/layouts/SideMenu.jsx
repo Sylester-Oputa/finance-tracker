8e4b757db6689a4470c280a28060222eb9abb57a
Sylester-Oputa <soputa42@gmail.com>
2025-07-17 21:11:45 +0100

    .env update

--- frontend/finance-tracker/src/components/layouts/SideMenu.jsx
+++ frontend/finance-tracker/src/components/layouts/SideMenu.jsx
@@ -29,7 +29,11 @@ const SideMenu = ({ activeMenu }) => {
       <div className="flex flex-col items-center justify-center gap-3 mt-3 mb-7">
         {user?.profileImageUrl ? (
           <img
-            src={user?.profileImageUrl || ""}
+            src={
+              user?.profileImageUrl
+                ? `${import.meta.env.VITE_API_BASE_URL}${user.profileImageUrl}`
+                : ""
+            }
             alt="Profile Image"
             className="w-20 h-20 bg-slate-400 rounded-full"
           />
