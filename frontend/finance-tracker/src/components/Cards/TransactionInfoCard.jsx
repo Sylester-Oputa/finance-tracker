cffb1844b759e91048d6571fc76375a0575bd2f3
Sylester-Oputa <soputa42@gmail.com>
2025-07-18 12:11:16 +0100

    commit

--- frontend/finance-tracker/src/components/Cards/TransactionInfoCard.jsx
+++ frontend/finance-tracker/src/components/Cards/TransactionInfoCard.jsx
@@ -21,11 +21,7 @@ const TransactionInfoCard = ({
   return (
     <div className="group relative flex items-center gap-4 mt-2 p-3 rounded-lg hover:bg-gray-300/60">
       <div className="w-12 h-12 flex items-center justify-center text-xl bg-gray-100 rounded-full">
-        {icon ? (
-          <img src={icon} alt={title} className="w-6 h-6" />
-        ) : (
-          <LuUtensils />
-        )}
+        {icon ? <span className="text-xl">{icon}</span> : <LuUtensils />}
       </div>
 
       <div className="flex-1 flex items-center justify-between">
