5d7a6297a0751ef5f208946d7c3aa316f0c59a2b
Sylester-Oputa <soputa42@gmail.com>
2025-07-18 13:22:52 +0100

    commit

--- frontend/finance-tracker/src/pages/Auth/Signup.jsx
+++ frontend/finance-tracker/src/pages/Auth/Signup.jsx
@@ -85,7 +85,7 @@ const Signup = () => {
         <form onSubmit={handleSignUp}>
           <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
 
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="flex flex-col gap-4">
             <Input
               value={fullName}
               onChange={({ target }) => setFullName(target.value)}
